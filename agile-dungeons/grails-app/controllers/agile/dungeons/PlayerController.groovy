869870ab56ad9147696b4c8953aaf6be21686c6a
Maximiliano Prystupiuk <mprystupiuk@fi.uba.ar>
2021-08-30 14:02:51 -0300

    Added greeting in player view read from the controller dictionary

--- agile-dungeons/grails-app/controllers/agile/dungeons/PlayerController.groovy
+++ agile-dungeons/grails-app/controllers/agile/dungeons/PlayerController.groovy
@@ -5,9 +5,14 @@ class PlayerController {
     def characterService
     def playerService
     def index(String username) {
-        if (!playerService.list().find({p -> p.username == username})){
+        def player = playerService.list().find({p -> p.username == username})
+        if (!player){
             response.status = 404
             render "Player not found"
         }
+        [
+            username: username,
+            characterName: player.character.name
+        ]
     }
 }
