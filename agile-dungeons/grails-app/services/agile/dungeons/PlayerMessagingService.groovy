480499dd90fa8966fe5198936aea886d463be954
Maximiliano Prystupiuk <mprystupiuk@fi.uba.ar>
2021-08-31 18:02:59 -0300

    Refactored logic in playerMessagingService

--- agile-dungeons/grails-app/services/agile/dungeons/PlayerMessagingService.groovy
+++ agile-dungeons/grails-app/services/agile/dungeons/PlayerMessagingService.groovy
@@ -4,12 +4,14 @@ import grails.gorm.transactions.Transactional
 
 @Transactional
 class PlayerMessagingService {
+    final SECONDS_BEFORE_ANOTHER_MESSAGE = 40
     def messageService
     def characterService
 
     def message(Long sourceCharacterId, Long destinationCharacterId, String message) { 
         def sourceCharacter = characterService.list().find({c -> c.id == sourceCharacterId})
         def destinationCharacter = characterService.list().find({c -> c.id == destinationCharacterId})
+
         if (!sourceCharacter.awake){
             throw new Exception("No se pueden enviar mensajes estando inconsciente.")
         }
@@ -20,22 +22,10 @@ class PlayerMessagingService {
 
         if (messageService.list().find({m -> m.approved == null && m.emisor != null && m.emisor.id == sourceCharacterId})) {
             throw new Exception("No se puede enviar un mensaje nuevo mientras tengas alguno pendiente...")
-        }        
-        def latests = messageService.list().findAll({m -> m.emisor != null && m.emisor.id == sourceCharacterId}).sort{it.date}
-        latests.reverse(true)        
-        
-        if (latests.any{it -> it}){
-            def latestSent = latests[0].date
-            def duration = groovy.time.TimeCategory.minus(
-                new Date(),
-                latestSent
-            );
-            //if (duration.days == 0 && duration.hours ==0 && duration.minutes < 10){
-            if (duration.days == 0 && duration.hours ==0 && duration.minutes == 0 && duration.seconds < 40){
-                println "Latest: ${latests[0].content} on ${duration.days}d ${duration.hours}h ${duration.minutes}m duration ago"
-                throw new Exception("Pasaron ${duration.hours}:${duration.minutes}:${duration.seconds} desde tu último mensaje. Sólo se puede enviar un mensaje cada 10 minutos.")                
-            }
-        }       
+        }    
+        if (!canSendMessage(sourceCharacterId)) {
+            throw new Exception("Todavía no estás habilitado para enviar otro mensaje. Debes esperar.")
+        }              
 
         def newMessage = new Message(
             date: new Date(),
@@ -45,33 +35,22 @@ class PlayerMessagingService {
             content: message)
         
         messageService.save(newMessage)
-
     }
 
     def askGM(Long sourceCharacterId, String message) {  
-        def sourceCharacter = characterService.list().find({c -> c.id == sourceCharacterId})        
+        def sourceCharacter = characterService.list().find({c -> c.id == sourceCharacterId})  
+
         if (!sourceCharacter.awake){
             throw new Exception("No se pueden enviar mensajes estando inconsciente.")
-        }        
+        }
 
         if (messageService.list().find({m -> m.approved == null && m.emisor != null && m.emisor.id == sourceCharacterId})) {
             throw new Exception("No se puede enviar un mensaje nuevo mientras tengas alguno pendiente...")
-        }        
-        def latests = messageService.list().findAll({m -> m.emisor != null && m.emisor.id == sourceCharacterId}).sort{it.date}
-        latests.reverse(true)        
-        
-        if (latests.any{it -> it}){
-            def latestSent = latests[0].date
-            def duration = groovy.time.TimeCategory.minus(
-                new Date(),
-                latestSent
-            );
-            //if (duration.days == 0 && duration.hours ==0 && duration.minutes < 10){
-            if (duration.days == 0 && duration.hours ==0 && duration.minutes == 0 && duration.seconds < 40){
-                println "Latest: ${latests[0].content} on ${duration.days}d ${duration.hours}h ${duration.minutes}m duration ago"
-                throw new Exception("Pasaron ${duration.hours}:${duration.minutes}:${duration.seconds} desde tu último mensaje. Sólo se puede enviar un mensaje cada 10 minutos.")                
-            }
-        }       
+        }
+
+        if (!canSendMessage(sourceCharacterId)){
+            throw new Exception("Todavía no estás habilitado para enviar otro mensaje. Debes esperar.")
+        }
 
         def newMessage = new Message(
             date: new Date(),
@@ -81,5 +60,24 @@ class PlayerMessagingService {
             content: message)
         
         messageService.save(newMessage)
+    }
+
+    def canSendMessage(Long sourceCharacterId){
+        def latests = messageService.list().findAll({m -> m.emisor != null && m.emisor.id == sourceCharacterId}).sort{it.date}
+        latests.reverse(true)        
+        
+        if (latests.any{it -> it}){
+            def latestSent = latests[0].date
+            def duration = groovy.time.TimeCategory.minus(
+                new Date(),
+                latestSent
+            );
+            
+            if (duration.days == 0 && duration.hours ==0 && duration.minutes == 0 && duration.seconds < SECONDS_BEFORE_ANOTHER_MESSAGE){
+                println "There's only been: ${duration.days}d ${duration.hours}h ${duration.minutes}m ${duration.seconds}s after the last message."
+                return false
+            }
+        } 
+        return true
     }
 }
